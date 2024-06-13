b8faecd70e61cb217849e8e1acfce681b049dae2
Sageyuva <yuvarajdhammure@gmail.com>
2024-06-13 19:17:10 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require ('mongoose')
+require('dotenv').config();
+
 
 // Initilaize App
 const app = express();
@@ -9,9 +11,11 @@ app.use(cors());
 
 // Connect to database
 
+const dbcon = process.env.DB_URL
+
 const connectTOdb = async() => {
 try {
-    await mongoose.connect("mongodb://localhost:27017/mernblog")
+    await mongoose.connect(dbcon)
     console.log("connected to DATA BASE")
 } catch (error) {
     console.log("failed to connect to data base")
